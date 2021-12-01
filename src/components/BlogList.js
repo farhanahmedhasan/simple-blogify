a372a951998ce6c870299c4a8c686eaad73e580e
farhanahmedhasan <achasandjmamunxyz2015@gmail.com>
2021-12-01 22:54:55 +0600

    Made a fake json db

--- src/components/BlogList.js
+++ src/components/BlogList.js
@@ -1,14 +1,14 @@
+import moment from 'moment';
+
 const BlogList = ({ blogs, title, gridCol }) => {
-  console.log(gridCol);
   const randomNumber = (max, min) => {
     const random = Math.floor(Math.random() * (max - min + 1)) + min;
     return random;
   };
-  // md:grid-cols-${md} lg:grid-cols-${lg} xl:grid-cols-${xl} 2xl:grid-cols-${Xxl}
+
   return (
     <>
       <h2 className='primaryHeading'>{title}</h2>
-
       <div className={`py-4 grid gap-4 grid-cols-1 ${gridCol}`}>
         {blogs.map((blog) => {
           const { title, author, date, content, id } = blog;
@@ -16,7 +16,7 @@ const BlogList = ({ blogs, title, gridCol }) => {
             <figure key={id} className='figure'>
               <div className='flex items-center justify-between'>
                 <h2 className='figure__title'> {title}</h2>
-                <span className='figure__date'>{date}</span>
+                <span className='figure__date'>{moment().add(date, 'days').calendar()}</span>
               </div>
 
               <span className='figure__author'>by {author}</span>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,19 +1,58 @@
-import { useState } from 'react';
+import { useState, useEffect, useCallback } from 'react';
 
 import BlogList from '../components/BlogList';
-import { blogsData } from '../assests/data/blogsData';
 
 const Home = () => {
-  const [blogs, setBlogs] = useState(blogsData);
+  const [blogs, setBlogs] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  const fetchBlogs = useCallback(async () => {
+    setLoading(true);
+    try {
+      const response = await fetch('http://localhost:8000/blogs');
+      const data = await response.json();
+
+      if (!response.ok) {
+        throw new Error(`Could not fetch blogs. Received ${response.status}`);
+      }
+
+      setBlogs(data);
+      setLoading(false);
+      setError(null);
+    } catch (error) {
+      setLoading(false);
+      setError(error.message);
+    }
+  }, []);
+
+  useEffect(() => {
+    setTimeout(() => {
+      fetchBlogs();
+    }, 3000);
+  }, [fetchBlogs]);
+
   return (
     <>
       <div className='container'>
-        <BlogList gridCol='md:grid-cols-2 2xl:grid-cols-3' blogs={blogs} title={`Your Daily Blogs!`} />
-        <BlogList
-          gridCol='md:grid-cols-2'
-          blogs={blogs.filter((blog) => blog.author === 'Noman Sheikh')}
-          title={`Noman's Blogs!`}
-        />
+        {error && <div className='text-4xl font-extrabold flex justify-center items-center h-screen'>{error}</div>}
+
+        {loading && <div className='text-6xl font-extrabold flex justify-center h-screen items-center'>Loading...</div>}
+
+        {blogs && blogs.length === 0 && (
+          <div className='text-6xl font-extrabold flex justify-center h-screen items-center'>No Blogs Found</div>
+        )}
+
+        {blogs && (
+          <>
+            <BlogList gridCol='md:grid-cols-2 2xl:grid-cols-3' blogs={blogs} title={`Your Daily Blogs!`} />
+            <BlogList
+              gridCol='md:grid-cols-2'
+              blogs={blogs.filter((blog) => blog.author === 'Noman Sheikh')}
+              title={`Noman's Blogs!`}
+            />
+          </>
+        )}
       </div>
     </>
   );
